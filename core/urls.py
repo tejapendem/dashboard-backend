753407dfcb7e923d2d51ee11cd64a43f6dd3f836
teja pendem <tejapendem@tejas-MacBook-Pro.local>
2025-07-07 14:53:03 +0530

    Add gunicorn for Render deployment

--- core/urls.py
+++ core/urls.py
@@ -25,5 +25,7 @@ urlpatterns = [
     path('api/tasks/', include('tasks.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/users/', include('users.urls')),  
+    path('api/tasks/', include('tasks.urls')),
 ]
 
